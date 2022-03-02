fa9db0a65ace85ba4b78571f542bd20cfd9a328d
Walter Cueva Chavez <waltercueva@gmail.com>
2022-03-01 19:02:39 -0500

    Create student.hpp

---
+++ student.hpp
@@ -0,0 +1,11 @@
+#pragma once // prevents multiple definitions
+// Exercise 1: implements function 'add', that adds two numbers
+int add(int a, int b)
+{
+    return 0; // TODO: fix
+}
+// Exercise 2: implements function 'mul', that multiplies two numbers
+int mul(int a, int b)
+{
+    return 0; // TODO: fix
+}
