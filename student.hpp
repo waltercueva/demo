8211e7f5d705dce47d7f387489754772c5d1a46b
Walter Cueva Chavez <waltercueva@gmail.com>
2022-06-24 06:25:39 -0500

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -2,7 +2,7 @@
 // Exercise 1: implements function 'add', that adds two numbers
 int add(int a, int b)
 {
-    return a+b; // TODO: fix
+    return 0; // TODO: fix
 }
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
